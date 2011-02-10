eba84e65eeae37f3a36b86223ef4e40f94eb5e35
Michael M Slusarz <slusarz@curecanti.org>
2011-02-10 16:56:13 -0700

    Step 1 of Horde_Block refactoring: H4-like class loading/naming

--- lib/Block/Bookmarks.php
+++ lib/Block/Bookmarks.php
@@ -1,9 +1,6 @@
 <?php
-
-$block_name = _("Bookmarks");
-
 /**
- * Implementation of Horde_Block api to show bookmarks.
+ * Show bookmarks.
  *
  * Copyright 2004-2011 The Horde Project (http://www.horde.org/)
  *
@@ -11,14 +8,23 @@ $block_name = _("Bookmarks");
  * did not receive this file, see http://www.horde.org/licenses/bsdl.php.
  *
  * @author  Joel Vandal <joel@scopserv.com>
- * @since   Trean 1.0
- * @package Horde_Block
  */
-class Horde_Block_Trean_bookmarks extends Horde_Block
+class Trean_Block_Bookmarks extends Horde_Block
 {
-    protected $_app = 'trean';
+    /**
+     */
     private $_folder = null;
 
+
+    /**
+     */
+    public function getName()
+    {
+        return _("Bookmarks");
+    }
+
+    /**
+     */
     protected  function _params()
     {
         require_once dirname(__FILE__) . '/../base.php';
@@ -26,7 +32,7 @@ class Horde_Block_Trean_bookmarks extends Horde_Block
         /* Get folders to display. */
         $folders = Trean::listFolders(Horde_Perms::READ);
         $default = null;
-        if (is_a($folders, 'PEAR_Error')) {
+        if ($folders instanceof PEAR_Error) {
             $GLOBALS['notification']->push(sprintf(_("An error occured listing folders: %s"), $folders->getMessage()), 'horde.error');
         } else {
             foreach ($folders as $key => $folder) {
@@ -37,46 +43,59 @@ class Horde_Block_Trean_bookmarks extends Horde_Block
             }
         }
 
-        return array('folder' => array('name' => _("Folder"),
-                                       'type' => 'enum',
-                                       'default' => $default,
-                                       'values' => $values),
-                     'bookmarks' => array('name' => _("Sort by"),
-                                          'type' => 'enum',
-                                          'default' => 'title',
-                                          'values' => array('title' => _("Title"),
-                                                            'highest_rated' => _("Highest Rated"),
-                                                            'most_clicked' => _("Most Clicked"))),
-                     'rows' => array('name' => _("Display Rows"),
-                                     'type' => 'enum',
-                                     'default' => '10',
-                                     'values' => array('10' => _("10 rows"),
-                                                       '15' => _("15 rows"),
-                                                       '25' => _("25 rows"))),
-                     'template' => array('name' => _("Template"),
-                                         'type' => 'enum',
-                                         'default' => '1line',
-                                         'values' => array('standard' => _("3 Line"),
-                                                           '2line' => _("2 Line"),
-                                                           '1line' => _("1 Line"))));
+        return array(
+            'folder' => array(
+                'name' => _("Folder"),
+                'type' => 'enum',
+                'default' => $default,
+                'values' => $values
+            ),
+            'bookmarks' => array(
+                'name' => _("Sort by"),
+                'type' => 'enum',
+                'default' => 'title',
+                'values' => array(
+                    'title' => _("Title"),
+                    'highest_rated' => _("Highest Rated"),
+                    'most_clicked' => _("Most Clicked")
+                )
+            ),
+            'rows' => array(
+                'name' => _("Display Rows"),
+                'type' => 'enum',
+                'default' => '10',
+                'values' => array(
+                    '10' => _("10 rows"),
+                    '15' => _("15 rows"),
+                    '25' => _("25 rows")
+                )
+            ),
+            'template' => array(
+                'name' => _("Template"),
+                'type' => 'enum',
+                'default' => '1line',
+                'values' => array(
+                    'standard' => _("3 Line"),
+                    '2line' => _("2 Line"),
+                    '1line' => _("1 Line")
+                )
+            )
+        );
     }
 
     /**
-     * The title to go in this block.
-     *
-     * @return string   The title text.
      */
     protected function _title()
     {
         global $registry;
 
         $folder = $this->_getFolder();
-        if (is_a($folder, 'PEAR_Error')) {
+        if ($folder instanceof PEAR_Error) {
             $name = $registry->get('name');
         } else {
             $name = $folder->get('name');
             if (!$name) {
-                $name = _("Bookmarks");
+                $name = $this->getName();
             }
         }
 
@@ -84,9 +103,6 @@ class Horde_Block_Trean_bookmarks extends Horde_Block
     }
 
     /**
-     * The content to go in this block.
-     *
-     * @return string   The content
      */
     protected function _content()
     {
@@ -96,7 +112,7 @@ class Horde_Block_Trean_bookmarks extends Horde_Block
         $template = TREAN_TEMPLATES . '/block/' . $this->_params['template'] . '.inc';
 
         $folder = $this->_getFolder();
-        if (is_a($folder, 'PEAR_Error')) {
+        if ($folder instanceof PEAR_Error) {
             return $folder;
         }
 
@@ -129,6 +145,8 @@ class Horde_Block_Trean_bookmarks extends Horde_Block
         return $html;
     }
 
+    /**
+     */
     private function _getFolder()
     {
         require_once dirname(__FILE__) . '/../base.php';
