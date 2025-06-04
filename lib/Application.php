d99b1e5332ca1aac138d85c62aeb23a45c8ee4f4
Ralf Lang <ralf.lang@ralf-lang.de>
2025-06-04 21:38:09 +0200

    Release 2.0.2  (API Version: 2.0.0)
    style: Apply php-cs-fixer php8.3+ rules

--- lib/Application.php
+++ lib/Application.php
@@ -34,7 +34,7 @@ class Trean_Application extends Horde_Registry_Application
 {
     /**
      */
-    public $version = 'H5 (2.0.2)';
+    public $version = '2.0.1';
 
     /**
      * Global variables defined:
