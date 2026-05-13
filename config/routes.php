9ccfc582cc8bfde96ded8f9cfe083b65edafb6ba
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-13 15:57:22 +0200

    refactor: Explicitly declare the default stack as a prestep to removing fallback logic

--- config/routes.php
+++ config/routes.php
@@ -1,17 +1,21 @@
 <?php
 
+use Horde\Core\Middleware\DefaultStack;
 use Horde\Trean\Controller\BrowseByTagController;
 use Horde\Trean\Controller\DeleteBookmarkController;
 use Horde\Trean\Controller\SaveBookmarkController;
 
 $mapper->buildRoute(uri: '/b/save', name: 'SaveBookmark')
     ->withController(SaveBookmarkController::class)
+    ->withMiddleware(DefaultStack::get())
     ->add();
 
 $mapper->buildRoute(uri: '/b/delete', name: 'DeleteBookmark')
     ->withController(DeleteBookmarkController::class)
+    ->withMiddleware(DefaultStack::get())
     ->add();
 
 $mapper->buildRoute(uri: '/tag/:tag', name: 'BrowseByTag')
     ->withController(BrowseByTagController::class)
+    ->withMiddleware(DefaultStack::get())
     ->add();
